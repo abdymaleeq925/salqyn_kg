1940b487553581023b008f886c94e74fc6a32d7d
abdymaleeq925 <abdymalikbatyrkulov@gmail.com>
2022-03-24 22:19:48 +0600

    add extends for html templates

--- accounts/views.py
+++ accounts/views.py
@@ -37,7 +37,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return render(request, 'register_done.html', {'new_user':new_user})
+            return render(request, 'register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegisterForm()
     return render(request, 'register.html', {'user_form': user_form})
